b672dc4be17ae783162ea2fe95e88ca452c6c1cc
Triggered1234 <adrian.enache03@e-uvt.ro>
2025-06-02 18:13:23 +0300

    TEST

--- test.R
+++ test.R
@@ -1,1 +1,36 @@
-cacat
+START - 1P
+Subiectul I
+Un produc˘ator testeaz˘a num˘arul de produse conforme din punct de vedere al calit˘at¸ii dintr-un lot de
+50 de buc˘at, i, astfel c˘a probabilitatea ca un produs s˘a fie conform standardelor de calitate este 0.7.
+a) Generat¸i 60 de valori pentru num˘arul de produse acceptate ˆıntr-un lot respectˆand repartit¸ia descris˘a
+anterior. Realizat¸i o histogram˘a cu un num˘ar adecvat de clase (precizat de voi ˆın comentariu),
+adaugat¸i un titlu acesteia s,
+i denumiri pentru axe. (2p)
+b) Estimat¸i probabilitatea ca num˘arul de produse conforme s˘a fie ˆıntre 30 s,
+i 40, pe baza datelor
+generate. (1p)
+c) Calculat¸i mediana ¸si decila 5 pentru num˘arul de produse conforme generate anterior, apoi scriet¸i
+ˆın comentarii interpretarea rezultatelor obt¸inute. (1p)
+Subiectul II
+a) ˆIntr-un program universitar, s-au colectat date privind timpii necesari pentru rezolvarea unui test
+de probabilit˘at¸i de c˘atre student, i. Datele referitoare la student, ii din anul curent sunt stocate ˆın
+fis,ierul studenti.csv. Se cunoas,te c˘a timpul mediu de rezolvare pentru student, ii din tot¸i anii este
+de 45 minute. Pentru un nivel de neˆıncredere de 2%, s˘a se verifice dac˘a student, ii din anul curent
+finalizeaz˘a testul mai rapid decˆat cei din anii precedent, i (specificat¸i toate etapele unui test statistic,
+                                                                dar ¸si interpretarea rezultatelor obt¸inute). (2p)
+b) Pentru un test asupra variant, ei timpilor de rezolvare, cu ipoteza alternativ˘a unilateral˘a (variant,a
+                                                                                                  a sc˘azut fat,˘a de cea anterioar˘a) s,
+i un nivel de ˆıncredere de 93%, avˆand un es,antion de dimensiune
+80, dac˘a valoarea calculat˘a statisticii este 21.21, aceasta este ˆın regiunea critic˘a asociat˘a acestui
+test? (1p)
+Subiectul III
+Setul de date trees cont,ine m˘asur˘atori despre diametrul, ˆın˘alt,imea s,
+i volumul arborilor.
+a) Reprezintat¸i grafic norul de puncte pentru diametru (Girth) s,
+i volum (Volume), adaugˆand un titlu
+s,
+i ad˘augat¸i o culoare graficului. (1p)
+b) Calculeaz˘a coeficientul de corelat, ie ˆıntre diametru s,i volum. Se poate afirma c˘a exist˘a corelat¸ie la
+nivelul e¸santionului? ˆIn caz afirmativ, precizat¸i tipul corelat¸iei ¸si verificat¸i dac˘a se ment¸ine corelat¸ia
+la nivelul populat¸iei. Justificat¸i r˘aspunsul. (1p)
+c) Scriet¸i ecuat¸ia de regresie liniar˘a ˆıntr-un comentariu pentru a estima volumul arborilor ˆın funct
